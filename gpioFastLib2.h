264b4b035e90500a8f873c07869b0212f4e451d3
cledic <clemente.dicaprio@gmail.com>
2014-12-26 17:34:34 +0100

    Create gpioFastLib2.h

---
+++ gpioFastLib2.h
@@ -0,0 +1,6 @@
+unsigned int init_memoryToIO(void);
+void writePortB(unsigned int uintData);
+void setGpioinOutput(unsigned int uintGpioB);
+void fastSetGpio(unsigned int uintGpioB);
+void fastClearGpio(unsigned int uintGpioB);
+

11ab66babec73aebe352fa37cfcc7eff1956debf
cledic <clemente.dicaprio@gmail.com>
2015-01-05 19:46:00 +0100

    Update acqua_install.sh

--- hr_timer/acqua_install.sh
+++ hr_timer/acqua_install.sh
@@ -2,3 +2,4 @@
 make -C ~/linux-3.10-acqua ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- M=`pwd` modules
 sshpass -p acmesystems scp ledpanel3v3.ko root@acqua-ip-address:/root
 sshpass -p acmesystems scp mod_hrt.ko root@acqua-ip-address:/root
+sshpass -p acmesystems scp mod_hrt2.ko root@acqua-ip-address:/root
